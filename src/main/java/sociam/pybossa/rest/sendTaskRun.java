e87432ae6155d3ceed6f1723c8deea7069c41790
SaudAljaloud <sza1g10@ecs.soton.ac.uk>
2016-02-28 21:32:03 +0000

    no need to contribution_id

---
+++ src/main/java/sociam/pybossa/rest/sendTaskRun.java
@@ -0,0 +1,68 @@
+package main.java.sociam.pybossa.rest;
+
+import java.io.InputStream;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import sociam.pybossa.config.Config;
+
+@Path("/sendTaskRun")
+public class sendTaskRun {
+	private static final Logger logger = Logger.getLogger(sendTaskRun.class);
+
+	@GET
+	@Produces("application/json")
+	public Response testing(@QueryParam("text") String text, @QueryParam("task_id") Integer task_id,
+			@QueryParam("project_id") Integer project_id, @QueryParam("contributor_name") String contributor_name,
+			@QueryParam("source") String source) throws JSONException {
+		try (InputStream stream = sendTaskRun.class.getResourceAsStream("/log4j.properties")) {
+			PropertyConfigurator.configure(stream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Config.reload();
+		JSONObject data = new JSONObject();
+		JSONObject status = new JSONObject();
+		if (text != null && task_id != null && project_id != null && contributor_name != null && source != null) {
+			logger.debug("receiving a GET request with the following data + text=" + text + " task_id=" + task_id
+					+ " project_id=" + project_id + " contributor_name=" + contributor_name + " source=" + source);
+			Boolean isInserted = sociam.pybossa.TaskCollector.insertTaskRun(text, task_id, project_id, contributor_name,
+					source);
+			if (isInserted) {
+				logger.info("TaskRun was inserted");
+
+				data.put("text", text);
+				data.put("task_id", task_id);
+				data.put("project_id", project_id);
+				data.put("contributor_name", contributor_name);
+				data.put("source", source);
+				status.put("data", data);
+				status.put("status", "success");
+				String result = status.toString();
+				return Response.status(200).entity(result).build();
+			} else {
+				logger.error("Task run could not be inserted");
+				status.put("status", "error");
+				return Response.status(500).entity(status.toString()).build();
+			}
+
+		} else {
+			logger.error("All parameters should be provided");
+			status.put("status", "Error: All parameters should be provided. text=" + text + "task_id=" + task_id
+					+ " project_id=" + project_id + " contributor_name=" + contributor_name + " source=" + source);
+			return Response.status(500).entity(status.toString()).build();
+		}
+
+	}
+
+}
